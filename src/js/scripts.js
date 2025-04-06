773f15fa519b5fcf019a013f9c193f068c7a4eb6
Habeeb Salami <1849920+habeeb-salami@users.noreply.github.com>
2025-04-06 10:57:22 +0100

    data request functionality added

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -15,15 +15,26 @@ function clearAlarm() {
     window.close();
 }
 $(function () {
-    chrome.notifications.create('notificationId', {
-        type: 'basic',
-        iconUrl: './images/ramadan.png', 
-        title: 'Salat Notification',
-        message: "Salam Alaikum, Time for Salat!!!"
-    });
-
+    // chrome.notifications.create('notificationId', {
+    //     type: 'basic',
+    //     iconUrl: './images/ramadan.png', 
+    //     title: 'Salat Notification',
+    //     message: "Salam Alaikum, Time for Salat!!!"
+    // });
+    fetchData();
     $("#sampleMinute").click(setAlarm);
     $("#min15").click(setAlarm);
     $("#min30").click(setAlarm);
     $("#cancelAlarm").click(clearAlarm);
 });
+
+
+
+async function fetchData() {
+    const res=await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
+    const record=await res.json();
+    document.getElementById("date").innerHTML=record.data[0].date;
+    document.getElementById("areaName").innerHTML=record.data[0].areaName;
+    document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
+    document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
+}
